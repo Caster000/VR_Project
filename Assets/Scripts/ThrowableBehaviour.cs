f2f1551ba7c5e7d79cc63518163006719cf25fb6
TheRoger18 <gaultier.geelen@gmail.com>
2022-10-18 17:57:35 +0200

    Rectified throwable behaviour

--- Assets/Scripts/ThrowableBehaviour.cs
+++ Assets/Scripts/ThrowableBehaviour.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ThrowableBehaviour : MonoBehaviour
 {
@@ -7,11 +8,14 @@ public class ThrowableBehaviour : MonoBehaviour
     private int scientistLayer;
     private Rigidbody throwableRigidbody;
 
+    private XRGrabInteractable grabController;
+
     private float power = 10.0f;
     private float radius = 5.0f;
     private float upforce = 1.0f;
 
-    [SerializeField] private float minimumThrownVelocity = 0.5f; //0.5 is chosen because it is considered sufficient to determine if a throwable has been thrown
+    [SerializeField] private float throwForce = 75f;
+    [SerializeField] private ForceMode throwMode = ForceMode.Impulse;
     [SerializeField] GameObject explosionPrefab;
 
     private void Awake()
@@ -22,11 +26,16 @@ public class ThrowableBehaviour : MonoBehaviour
         power = GameConfigLoader.Instance.gameConfig.ExplosionPower > 0.0f ? GameConfigLoader.Instance.gameConfig.ExplosionPower : power;
         radius = GameConfigLoader.Instance.gameConfig.RadiusExplosion > 0.0f ? GameConfigLoader.Instance.gameConfig.RadiusExplosion : radius;
         upforce = GameConfigLoader.Instance.gameConfig.ExplosionUpforce > 0.0f ? GameConfigLoader.Instance.gameConfig.ExplosionUpforce : upforce;
+        grabController = GetComponent<XRGrabInteractable>();
     }
 
     public void Throw()
     {
-        isThrown = throwableRigidbody?.velocity.magnitude > minimumThrownVelocity; 
+        // Debug.Log(throwableRigidbody.velocity.magnitude);
+        // isThrown = throwableRigidbody.velocity.magnitude > minimumThrownVelocity;
+        isThrown = true;
+        grabController.interactionManager.CancelInteractableSelection(grabController);
+        throwableRigidbody.AddForce(grabController.transform.forward * throwForce, throwMode);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -37,12 +46,14 @@ public class ThrowableBehaviour : MonoBehaviour
 
     private void Explode()
     {
+        Debug.Log("Explode");
         Transform objectTransform = gameObject.transform;
         Vector3 position = objectTransform.position;
 
         // Add particles
-        Instantiate(explosionPrefab, position, objectTransform.rotation);
-
+        GameObject g = Instantiate(explosionPrefab, position, objectTransform.rotation);
+        Destroy(g, 3);
+        
         // Push everything around & damage players
         Collider[] hitColliders = Physics.OverlapSphere(position, radius);
         GameObject currentTreatedObject;
@@ -55,7 +66,7 @@ public class ThrowableBehaviour : MonoBehaviour
             if(currentRigidbody)
                 currentRigidbody.AddExplosionForce(power, position, radius, upforce, ForceMode.Impulse);
 
-            if (currentTreatedObject.layer == scientistLayer)
+            if (currentTreatedObject.layer == scientistLayer) //TODO friendly fire ?
                 currentTreatedObject.GetComponent<UserManager>().TakeDamage();
         }
         
